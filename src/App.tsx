7cd88bc066179cb364251e186e6071067dad40ee
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-10-23 15:43:04 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import {MessageList} from './components/MessageList'
+import {LoginBox} from './components/LoginBox'
+
+import styles from './App.module.scss'
 
 export function App() {
  
 
   return (
-    <h1>Hello World</h1>
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+      
+    </main>
   )
 }
 
